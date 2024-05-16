2924dee490d9d1e9f7943e62e07132fb5e803474
debora.fuentes <bibifuentespiris@gmail.com>
2024-05-16 17:10:41 +0200

    Initial commit

---
+++ src/main/java/com/softtek/dao/response/JwtAuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.softtek.dao.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JwtAuthenticationResponse { private String token;
+}
